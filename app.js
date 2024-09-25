e83eec8f4912112adf012f8d521263e529d912a7
Diether012524 <labisdiether345@gmail.com>
2024-09-25 17:46:34 +0800

    Added arithmetic and logical operators

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ let str = "Hello, World!";
 let isEven = false;
 let array = [1, 2, 3, 4, 5]; 
 let obj = { name: "Alice", age: 30 }; 
+
+//Arithmetic and Logical Operators
+let sum = num + 5; 
+let isGreater = num > 5; 
