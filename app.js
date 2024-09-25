cd57be70e45db4620ff841afe73d49550c02ed03
Diether012524 <labisdiether345@gmail.com>
2024-09-25 17:48:56 +0800

    Added conditional statements

--- app.js
+++ app.js
@@ -8,3 +8,33 @@ let obj = { name: "Alice", age: 30 };
 //Arithmetic and Logical Operators
 let sum = num + 5; 
 let isGreater = num > 5; 
+
+//Conditional Statements
+//if else
+function checkEvenOdd(number) {
+    if (number % 2 === 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+
+//switch
+function describeNumber(number) {
+    switch (true) {
+        case (number < 0):
+            return "Negative";
+        case (number === 0):
+            return "Zero";
+        case (number > 0):
+            return "Positive";
+        default:
+            return "Unknown";
+    }
+}
+
+//Loops
+console.log("Numbers from 1 to 5:");
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
