06d0a513a32e27a1b75663059695f8a8d8b2591a
Diether012524 <labisdiether345@gmail.com>
2024-10-06 06:48:35 +0800

    Added constructor

--- applet-3.js
+++ applet-3.js
@@ -1,3 +1,41 @@
 class DataLogger{
+    constructor(buttonId, cardContainerId, clearButton, logCountId) {
+
+  
+        this.logButton = document.getElementById(buttonId);
+        this.cardContainerId = document.getElementById(cardContainerId);
+        this.clearButton = document.getElementById(clearButton);
+        this.logButton = document.getElementById(logCountId);
+        this.loggedData = [];
+    
+        this.logButton.addEventListener('click', () => this.loggedData());
+        this.clearButton.addEventListener('click', () => this.clearButton());
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
