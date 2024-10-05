074fcc8d9d70bb058e532c7a7a7bd5292e22871d
Diether012524 <labisdiether345@gmail.com>
2024-10-05 20:25:51 +0800

    Added const Mymap

--- applet-2.js
+++ applet-2.js
@@ -1,6 +1,5 @@
 class LeafletMap {
 
-    
     constructor(containerId, center, zoom) {
         this.map = L.map(containerId).setView(center, zoom);
         this.initTileLayer();
@@ -11,22 +10,23 @@ class LeafletMap {
             maxZoom: 19,
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(this.map);
-    
-        addMarker(lat, lng, message) {
-            const marker = L.marker([lat, lng]).addTo(this.map);
-            marker.bindPopup(message);
-        }
-        loadMarkersFromJson(url) {
-            fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                    data.forEach(marker => {
-                        this.addMarker(marker.latitude, marker.longitude, marker.message);
-                    });
-                })
-                .catch(error => console.error('Error loading markers:', error));
+    }
 
+    addMarker(lat, lng, message) {
+        const marker = L.marker([lat, lng]).addTo(this.map);
+        marker.bindPopup(message);
+    }
 
-
+    loadMarkersFromJson(url) {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(marker => {
+                    this.addMarker(marker.latitude, marker.longitude, marker.message);
+                });
+            })
+            .catch(error => console.error('Error loading markers:', error));
+    }
 }
-}
+
+const Mymap = new LeafletMap('map', [8.359735, 124.869206], 18);
