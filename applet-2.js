8791a84136a5d41025bb28cb8905d5145dbc912f
Diether012524 <labisdiether345@gmail.com>
2024-10-06 08:33:29 +0800

    Added my map

--- applet-2.js
+++ applet-2.js
@@ -115,10 +115,15 @@ class LeafletMap {
         });
         this.displayLogCount();
     }
-
+}
+    const Mymap = new LeafletMap('map', [8.359735, 124.869206], 18);
+    Mymap.loadMarkersFromJson('applet-2.json');
+    
+    document.addEventListener('DOMContentLoaded', () => {
+        Mymap.displayLogCount();
+    });
 
   
 
 
 
-}
