4cea74bbfbdba025261f5ec871c2dae4ad0927a0
Aldrinsain <angelasain86@gmail.com>
2024-10-11 07:47:03 +0800

    Added filterStudents method and objects

--- applet-4.js
+++ applet-4.js
@@ -41,6 +41,18 @@ class studentList{
         this.renderStudentList(this.students, studentSearchListContainer);
     }
 
+    filterStudents(query, searchListContainer) {
+        const filteredStudents = this.students.filter(student => {
+            const fullName = `${student.student_name} ${student.student_program}`;
+            return fullName.toLowerCase().includes(query.toLowerCase());
+        });
+
+        searchListContainer.innerHTML = '';
+
+        this.renderStudentList(filteredStudents, searchListContainer);
+    }
+    
+
 
 
 
