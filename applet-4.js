7ede1d8da01d537dec8ed6e7d538dda548143579
Aldrinsain <angelasain86@gmail.com>
2024-10-11 07:40:40 +0800

    Modified a init method and added a object of fetchData

--- applet-4.js
+++ applet-4.js
@@ -5,12 +5,21 @@ class studentList{
     this.students = [];
     this.init();
     
-} 
+}
+
+    async init() {
+    await this.fetchData();
+    this.renderStudentList(this.students); 
+    this.bindSearchEvent();
+}
 
      async fetchData() {
-        await this.fetchData();
-        this.renderStudentList(this.students); 
-        this.bindSearchEvent();
+        try {
+            const response = await fetch(this.dataUrl);
+            this.students = await response.json();
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
     }
 
 
