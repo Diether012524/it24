1853baa8cc1342796c2f5d8ea10dd77ee1dc0a8a
Diether012524 <labisdiether345@gmail.com>
2024-10-26 22:48:21 +0800

    Added reset editing method

--- applet-6.js
+++ applet-6.js
@@ -58,6 +58,11 @@ class TodoList {
         this.addButton.textContent = 'Update';
     }
 
+    resetEditing() {
+        this.editingIndex = -1;
+        this.addButton.textContent = 'Add';
+    }
+    
 
 
 
